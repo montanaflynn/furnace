23d81fbf34914eb5a6428aad34fd13b5cd13418a
Balazs Kocso <balazs.kocso@balabit.com>
2018-12-17 13:23:01 +0100

    furnace/config.py: create namedtuple for recurring config options
    This solution is more pythonic, than use dict of dicts.

--- furnace/config.py
+++ furnace/config.py
@@ -17,10 +17,16 @@
 # along with Furnace.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+from collections import namedtuple
+
 from pathlib import Path
 
 from .libc import CLONE_NEWPID, CLONE_NEWCGROUP, CLONE_NEWIPC, CLONE_NEWUTS, CLONE_NEWNS, CLONE_NEWNET, \
     MS_NOSUID, MS_NOEXEC, MS_NODEV, MS_RDONLY, MS_STRICTATIME
+
+
+ContainerMount = namedtuple('ContainerMount', ['destination', 'type', 'source', 'flags', 'options'])
+ContainerDeviceNode = namedtuple('ContainerDeviceNode', ['name', 'major', 'minor'])
 
 HOSTNAME = 'localhost'
 
@@ -34,96 +40,100 @@ NAMESPACES = {
 }
 
 CONTAINER_MOUNTS = [
-    {
-        "destination": Path("/proc"),
-        "type": "proc",
-        "source": "proc"
-    },
-    {
-        "destination": Path("/dev"),
-        "type": "tmpfs",
-        "source": "tmpfs",
-        "flags": MS_NOSUID | MS_STRICTATIME,
-        "options": [
+    ContainerMount(
+        destination=Path("/proc"),
+        type="proc",
+        source="proc",
+        flags=None,
+        options=None,
+    ),
+    ContainerMount(
+        destination=Path("/dev"),
+        type="tmpfs",
+        source="tmpfs",
+        flags=MS_NOSUID | MS_STRICTATIME,
+        options=[
             "mode=755",
-            "size=65536k"
-        ]
-    },
-    {
-        "destination": Path("/dev/pts"),
-        "type": "devpts",
-        "source": "devpts",
-        "flags": MS_NOSUID | MS_NOEXEC,
-        "options": [
+            "size=65536k",
+        ],
+    ),
+    ContainerMount(
+        destination=Path("/dev/pts"),
+        type="devpts",
+        source="devpts",
+        flags=MS_NOSUID | MS_NOEXEC,
+        options=[
             "newinstance",
             "ptmxmode=0666",
             "mode=0620",
-            "gid=5"
-        ]
-    },
-    {
-        "destination": Path("/dev/shm"),
-        "type": "tmpfs",
-        "source": "shm",
-        "flags": MS_NOSUID | MS_NOEXEC | MS_NODEV,
-        "options": [
+            "gid=5",
+        ],
+    ),
+    ContainerMount(
+        destination=Path("/dev/shm"),
+        type="tmpfs",
+        source="shm",
+        flags=MS_NOSUID | MS_NOEXEC | MS_NODEV,
+        options=[
             "mode=1777",
-            "size=65536k"
-        ]
-    },
-    {
-        "destination": Path("/dev/mqueue"),
-        "type": "mqueue",
-        "source": "mqueue",
-        "flags": MS_NOSUID | MS_NOEXEC | MS_NODEV,
-    },
-    {
-        "destination": Path("/sys"),
-        "type": "sysfs",
-        "source": "sysfs",
-        "flags": MS_NOSUID | MS_NOEXEC | MS_NODEV | MS_RDONLY,
-    },
-    {
-        "destination": Path("/run"),
-        "type": "tmpfs",
-        "source": "shm",
-        "flags": MS_NOSUID | MS_NOEXEC | MS_NODEV,
-        "options": [
+            "size=65536k",
+        ],
+    ),
+    ContainerMount(
+        destination=Path("/dev/mqueue"),
+        type="mqueue",
+        source="mqueue",
+        flags=MS_NOSUID | MS_NOEXEC | MS_NODEV,
+        options=None,
+    ),
+    ContainerMount(
+        destination=Path("/sys"),
+        type="sysfs",
+        source="sysfs",
+        flags=MS_NOSUID | MS_NOEXEC | MS_NODEV | MS_RDONLY,
+        options=None,
+    ),
+    ContainerMount(
+        destination=Path("/run"),
+        type="tmpfs",
+        source="shm",
+        flags=MS_NOSUID | MS_NOEXEC | MS_NODEV,
+        options=[
             "mode=1777",
-            "size=65536k"
-        ]
-    },
+            "size=65536k",
+        ],
+    ),
 ]
 
 CONTAINER_DEVICE_NODES = [
-    {
-        "name": "null",
-        "major": 1,
-        "minor": 3,
-    },
-    {
-        "name": "zero",
-        "major": 1,
-        "minor": 5,
-    },
-    {
-        "name": "full",
-        "major": 1,
-        "minor": 7,
-    },
-    {
-        "name": "tty",
-        "major": 5,
-        "minor": 0,
-    },
-    {
-        "name": "random",
-        "major": 1,
-        "minor": 8,
-    },
-    {
-        "name": "urandom",
-        "major": 1,
-        "minor": 9,
-    },
+    ContainerDeviceNode(
+        name="null",
+        major=1,
+        minor=3,
+    ),
+    ContainerDeviceNode(
+        name="zero",
+        major=1,
+        minor=5,
+    ),
+    ContainerDeviceNode(
+        name="full",
+        major=1,
+        minor=7,
+    ),
+    ContainerDeviceNode(
+        name="tty",
+        major=5,
+        minor=0,
+    ),
+    ContainerDeviceNode(
+        name="random",
+        major=1,
+        minor=8,
+    ),
+    ContainerDeviceNode(
+        name="urandom",
+        major=1,
+        minor=9,
+    ),
 ]
